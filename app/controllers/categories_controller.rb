4dbd37d2faa41f7e6d8b3812d8f337bc7e1520fe
Rick Cid <rick@hispand.com>
2013-10-19 10:46:18 -0700

    Added Votes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,28 @@
 class CategoriesController < ApplicationController
-  before_action :require_user
+  before_action :require_user, except: [:show]
   
   def new
     @category = Category.new
   end
 
   def create
-    @category = Category.new(params.require(:category).permit(:name)) 
+    @category = Category.new(category_params) 
     if @category.save
       flash[:notice] = "Category was created!"
       redirect_to posts_path
     else
       render :new
     end
-
   end
+
+  def show
+    category = Category.find(params[:id])
+    @posts = category.posts
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
